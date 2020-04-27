fc8949218647a68ce92878c1eaf84b320480a2e5
Ajith Kumar <kumarajith1996@gmail.com>
2020-04-27 12:05:21 +0530

    Base commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const { prefix, token } = require('./config.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.on('message', message => {
+	if (message.content === '!ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
+});
+client.login(token);
